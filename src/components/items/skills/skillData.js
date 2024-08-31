9b97431757be101002c19743c726e44ca9631f47
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-08-31 16:22:42 +0000

    Key added

--- src/components/items/skills/skillData.js
+++ src/components/items/skills/skillData.js
@@ -1,52 +1,63 @@
-const skillData = [
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922634/html5_ebxj4i.png",
-      languageName: "Html"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922691/cssLogo_ren2ec.png",
-      languageName: "Css"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922688/jsLogo_talofw.png",
-      languageName: "JavaScript"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922716/material-Logo_eaejac.png",
-      languageName: "Material UI"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922736/next-js-seeklogo_trs43v.png",
-      languageName: "Next Js"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922781/reactjs-logo-icon_q06uez.png",
-      languageName: "React Js"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922771/tailwind-Logo_o27mh9.png",
-      languageName: "Tailwind css"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922822/OIP_wdyunl.jpg",
-      languageName: "Typescript"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922825/python_jzeacp.png",
-      languageName: "Python"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922844/express_q0zlod.png",
-      languageName: "Express Js"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922864/cplus_xhdafl.png",
-      languageName: "C++"
-    },
-    {
-      imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922881/mongodb_ahxx3e.png",
-      languageName: "Mongodb"
-    }
-  ];
-  
- export default skillData 
+const skills = [
+  {
+    id: 0,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922634/html5_ebxj4i.png",
+    languageName: "Html"
+  },
+  {
+    id: 1,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922691/cssLogo_ren2ec.png",
+    languageName: "Css"
+  },
+  {
+    id: 2,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922688/jsLogo_talofw.png",
+    languageName: "JavaScript"
+  },
+  {
+    id: 3,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922716/material-Logo_eaejac.png",
+    languageName: "Material UI"
+  },
+  {
+    id: 4,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922736/next-js-seeklogo_trs43v.png",
+    languageName: "Next Js"
+  },
+  {
+    id: 5,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922781/reactjs-logo-icon_q06uez.png",
+    languageName: "React Js"
+  },
+  {
+    id: 6,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922771/tailwind-Logo_o27mh9.png",
+    languageName: "Tailwind css"
+  },
+  {
+    id: 7,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922822/OIP_wdyunl.jpg",
+    languageName: "Typescript"
+  },
+  {
+    id: 8,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922825/python_jzeacp.png",
+    languageName: "Python"
+  },
+  {
+    id: 9,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922844/express_q0zlod.png",
+    languageName: "Express Js"
+  },
+  {
+    id: 10,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922864/cplus_xhdafl.png",
+    languageName: "C++"
+  },
+  {
+    id: 11,
+    imgUrl: "https://res.cloudinary.com/dttek3gqg/image/upload/v1724922881/mongodb_ahxx3e.png",
+    languageName: "Mongodb"
+  }
+];
+export default skillData 
--- src/components/items/skills/skills.jsx
+++ src/components/items/skills/skills.jsx
@@ -7,11 +7,12 @@ const Skills = ({animation}) => {
   return (
     
     <section className='lg:mx-20 md:mx-15 sm:mx-0' data-aos={animation}>
-    <div className='flex flex-col w-full mb-5 mt-10 lg:p-12 md:p-6 sm:p-3 p-0 '>
+    <div className='flex flex-col w-full  mb-5 mt-10 lg:p-12 md:p-6 sm:p-3 p-0 '>
        
         <div className="flex flex-row my-1  justify-between ">
-          {skillData.slice(0,4).map((data)=>{
+          {skillData.slice(0,4).map((data,key)=>{
            return  <SkillItem
+           key={key(data.id)}
              imgUrl={data.imgUrl}
              lanuageName={data.languageName}
              />
@@ -19,23 +20,26 @@ const Skills = ({animation}) => {
          
         </div>
         <div className="flex flex-row my-1  justify-evenly ">
-        {skillData.slice(4,5).map((data)=>{
+        {skillData.slice(4,5).map((data,key)=>{
            return  <SkillItem
+           key={key(data.id)}
              imgUrl={data.imgUrl}
              lanuageName={data.languageName}
              cssAdd={"icon-dark"}
              />
           })}
-        {skillData.slice(5,8).map((data)=>{
+        {skillData.slice(5,8).map((data,key)=>{
            return  <SkillItem
+           key={key(data.id)}
              imgUrl={data.imgUrl}
              lanuageName={data.languageName}
              />
           })}
         </div>
         <div className="flex flex-row my-1  justify-between ">
-        {skillData.slice(8,12).map((data)=>{
+        {skillData.slice(8,12).map((data,key)=>{
            return  <SkillItem
+           key={key(data.id)}
              imgUrl={data.imgUrl}
              lanuageName={data.languageName}
              />
