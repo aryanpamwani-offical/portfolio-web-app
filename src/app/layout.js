bbb97df125da92716e1d2a224b20ff5c74392777
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-09-11 13:04:05 +0000

    Removing errors

--- src/app/layout.js
+++ src/app/layout.js
@@ -59,7 +59,7 @@ containerId={"GTM-K8R3BDDL"}
  
       
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K8R3BDDL"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
 
         </body>
     </html>
