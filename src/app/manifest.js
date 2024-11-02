70c514b865863452fada9ba3ed3f3b0299178f72
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-11-02 15:31:45 +0000

    Adding Robots,Manifest

---
+++ src/app/manifest.js
@@ -0,0 +1,18 @@
+export default function manifest() {
+    return {
+      name: `Aryan Pamwani `,
+      short_name: `Aryan Pamwani `,
+      description: 'Hi, I’m Aryan Pamwani, a 21-year-old B.Pharma student at RGPV (Rajiv Gandhi Proudyogiki Vishwavidyalaya). Passionate about UI/UX design, I love exploring creative solutions using tools like Figma. When I’m not studying, I’m immersed in the world of design, learning about user experience and creating intuitive interfaces.',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#fff',
+      theme_color: '#fff',
+      icons: [
+        {
+          src: '/favicon.ico',
+          sizes: 'any',
+          type: 'image/x-icon',
+        },
+      ],
+    }
+  }
