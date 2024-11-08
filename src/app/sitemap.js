461419d522f120a2268d02a73c40c3e15f26ec43
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-11-08 08:15:02 +0000

    Updating Url

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,31 +1,31 @@
 export default function sitemap() {
     return [
       {
-        url: 'https://aryanpamwani.ine/',
+        url: 'https://aryanpamwani.in/',
         lastModified: new Date(),
         changeFrequency: 'yearly',
         priority: 1,
       },
       {
-        url: 'https://aryanpamwani.ine/about',
+        url: 'https://aryanpamwani.in/about',
         lastModified: new Date(),
         changeFrequency: 'monthly',
         priority: 0.8,
       },
       {
-        url: 'https://aryanpamwani.ine/skills',
+        url: 'https://aryanpamwani.in/skills',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.5,
       },
       {
-        url: 'https://aryanpamwani.ine/projects',
+        url: 'https://aryanpamwani.in/projects',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.3,
       },
       {
-        url: 'https://aryanpamwani.ine/contact',
+        url: 'https://aryanpamwani.in/contact',
         lastModified: new Date(),
         changeFrequency: 'weekly',
         priority: 0.3,
