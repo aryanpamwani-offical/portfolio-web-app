4a084d758d49c2107cc312aa6b4621376dd94c76
aryanpamwani-offical <aryanpamwani@outlook.com>
2025-04-19 15:24:10 +0000

    gdgfg

--- src/components/items/HeroSection/ImageComp.jsx
+++ src/components/items/HeroSection/ImageComp.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const ImageComp = ({animation}) => {
   return (
-    <div className="flex flex-col lg:w-4/4 content-center justify-center items-center m-auto lg:mr-20  w-3/4 p-5" data-aos={animation}>
+    <div className="flex flex-col  content-center justify-center items-center m-auto lg:mr-20  w-3/4 p-5" data-aos={animation}>
    
-   <Image src="/logo.jpg" width={300} height={300} alt='hero' className='rounded-2xl  min-w-fit max-w-[300px] ' priority={true}/>
+   <Image src="/logo.jpg" width={300} height={300} alt='hero' className='rounded-2xl  ' priority={true}/>
         
 </div>
   )
