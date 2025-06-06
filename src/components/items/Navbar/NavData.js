156dadd07a7550d242e903914fdf4c113237b3a6
aryanpamwani-offical <aryanpamwani@outlook.com>
2025-06-06 16:28:09 +0000

    Reorder AI navigation item for improved structure

--- src/components/items/Navbar/NavData.js
+++ src/components/items/Navbar/NavData.js
@@ -16,12 +16,12 @@ export const navItems=[
         location:"/projects"
       } ,
       {
+       name:"AI",
+       location:"https://ai.aryanpamwani.in"
+     } ,
+      {
         name:"Blog",
         location:"https://blog.aryanpamwani.in"
-      } ,
-       {
-        name:"AI",
-        location:"https://ai.aryanpamwani.in"
       } ,
     {
       name:"Get in Touch",
